18436eb2d3a820647dd3b334809bf979cee4f96f
Rodrigo Noal <rodrigonoal@gmail.com>
2022-08-06 15:35:36 -0300

    db separado & estrutura de arquivos melhorada

--- DB/knex.js
+++ DB/knex.js
@@ -1,9 +1,5 @@
 require('dotenv').config()
 
-const atributos = require("../Datasets/atributos")
-const proezas = require("../Datasets/proezas")
-const especificacoes = require("../Datasets/especificacoes")
-
 
 const knex = require('knex')({
     client: 'pg',
@@ -16,44 +12,5 @@ const knex = require('knex')({
         database: process.env.DB_DATABASE
     }
 });
-
-function cleanup() {
-    for(let proeza of proezas) {
-        if(proeza.descricao.charAt(proeza.descricao.length-1) != "."){
-            proeza.descricao = proeza.descricao + "."
-        }
-        if(proeza.especifica) {
-            const esp = especificacoes.find(esp => esp.proeza === proeza.nome);
-            proeza.especificacao = esp.especificacao;
-        }
-    }
-    console.log("Descriptions cleaned up")
-    return
-}
-
-async function populateDb() {
-    try {
-        for (let atributo of atributos) {
-            await knex("atributos").insert(atributo)
-        }
-
-        const atbArray = await knex.select("id", "nome").table('atributos');
-
-        for (let proeza of proezas) {
-            proeza.atributoid = atbArray.find(atb => atb.nome === proeza.atributo).id
-            delete proeza["atributo"];
-
-            await knex("proezas").insert(proeza)
-        }
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
-    console.log("All objects inserted!")
-    return
-}
-
-cleanup();
-populateDb();
 
 module.exports = knex;
---
+++ DB/popularDB.js
@@ -0,0 +1,54 @@
+const knex = require("./knex");
+
+const atributos = require("../Datasets/atributos")
+const proezas = require("../Datasets/proezas")
+const especificacoes = require("../Datasets/especificacoes")
+
+function cleanup() {
+    for (let proeza of proezas) {
+        if (proeza.descricao.charAt(proeza.descricao.length - 1) != ".") {
+            proeza.descricao = proeza.descricao + "."
+        }
+        if (proeza.especifica) {
+            const esp = especificacoes.find(esp => esp.proeza === proeza.nome);
+            proeza.especificacao = esp.especificacao;
+        }
+    }
+    console.log("Descriptions cleaned up")
+    return
+}
+
+async function popularDb() {
+    try {
+        await knex("proezas")
+            .where("nome", "!=", "")
+            .del()
+
+        await knex("atributos")
+            .where("nome", "!=", "")
+            .del()
+            
+        await knex.raw("ALTER SEQUENCE atributos_id_seq RESTART WITH 1; ALTER SEQUENCE proezas_id_seq RESTART WITH 1;")
+
+        for (let atributo of atributos) {
+            await knex("atributos").insert(atributo)
+        }
+
+        const atbArray = await knex.select("id", "nome").table('atributos');
+
+        for (let proeza of proezas) {
+            proeza.atributoid = atbArray.find(atb => atb.nome === proeza.atributo).id
+            delete proeza["atributo"];
+
+            await knex("proezas").insert(proeza)
+        }
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+    console.log("All objects inserted!")
+    return
+}
+
+cleanup();
+popularDb();
