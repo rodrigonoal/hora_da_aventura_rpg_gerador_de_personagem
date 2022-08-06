869afe609434de7775a30f92a97f9dee81ce1dc3
Rodrigo Noal <rodrigonoal@gmail.com>
2022-08-06 16:29:27 -0300

    especificacoes incluidas

---
+++ Datasets/especificacoes.js
@@ -0,0 +1,80 @@
+const especificacoes = [
+    {
+        "proeza": "Mago",
+        "especificacao": "Escola de magia"
+    },
+    {
+        "proeza": "Arma incorporada",
+        "especificacao": "Arma & parte do corpo"
+    },
+    {
+        "proeza": "Gênio Inventor",
+        "especificacao": "Tipo especifico de invenção"
+    },
+    {
+        "proeza": "Partes mecânicas",
+        "especificacao": "Parte do corpo robotizada"
+    },
+    {
+        "proeza": "Inimigo preferido",
+        "especificacao": "Criatura vulnerável"
+    },
+    {
+        "proeza": "Letal",
+        "especificacao": "Arma escolhida"
+    },
+    {
+        "proeza": "Treinado",
+        "especificacao": "Arma escolhida"
+    },
+    {
+        "proeza": "Imune",
+        "especificacao": "Dano específico"
+    },
+    {
+        "proeza": "Regeneração",
+        "especificacao": "Dano específico"
+    },
+    {
+        "proeza": "Resistente",
+        "especificacao": "Dano específico"
+    },
+    {
+        "proeza": "Autoridade",
+        "especificacao": "Campo específico"
+    },
+    {
+        "proeza": "Aterrorizar",
+        "especificacao": "Forma de aterrorizar"
+    },
+    {
+        "proeza": "Metamorfo",
+        "especificacao": "Coisa para se transformar"
+    },
+    {
+        "proeza": "Popular",
+        "especificacao": "Grupo específico"
+    },
+    {
+        "proeza": "Elemental",
+        "especificacao": "Elemento específico"
+    },
+    {
+        "proeza": "Enfeitiçar",
+        "especificacao": "Forma de enfeitiçar & efeito do feitiço"
+    },
+    {
+        "proeza": "Maldição",
+        "especificacao": "No que consiste a maldição, como se transmite e como pode ser revertida ou detida"
+    },
+    {
+        "proeza": "Raio enfeitiçante",
+        "especificacao": "Efeito do feitiço"
+    },
+    {
+        "proeza": "Transformação",
+        "especificacao": "Coisa na qual transforma os outros"
+    }
+]
+
+module.exports = especificacoes;
