197cd89ce10ca3c857f19dea509e800841c1be49
Rodrigo Noal <rodrigonoal@gmail.com>
2022-08-06 16:17:51 -0300

    gerador de personagens salvando personagens

--- gerarPersonagem.js
+++ gerarPersonagem.js
@@ -1,59 +1,67 @@
+const fs = require('fs').promises;
+
 const embaralharArray = require("./Funcoes/embaralharArray");
 const Personagem = require("./Classes/Personagem");
 const knex = require("./DB/knex");
 
-async function gerarPersonagem(forte = 5, medio = 4, fraco = 3, minimo = 2) {
+const gerarPersonagem = async (forte = 5, medio = 4, fraco = 3, minimo = 2) => {
     const personagem = new Personagem();
 
-    const atributosArray = await knex.select("id", "nome").table('atributos');
-    const atributosEmbaralhados = embaralharArray(atributosArray);
+    try {
+        const atributosArray = await knex.select("id", "nome").table('atributos');
+        const atributosEmbaralhados = embaralharArray(atributosArray);
 
-    for (let i = 0; i < atributosEmbaralhados.length; i++) {
-        switch (i) {
-            case 0:
-                atributosEmbaralhados[i].valor = forte;
-                break;
-            case 1:
-                atributosEmbaralhados[i].valor = medio;
-                break;
-            case 2:
-                atributosEmbaralhados[i].valor = fraco;
-                break;
-            default:
-                atributosEmbaralhados[i].valor = minimo
-        }
-
-        personagem.atributos.push(atributosEmbaralhados[i])
-
-        // Define Proezas
-        if (i <= 2) {
-            let pontosDeProeza = i === 2 ? 1 : Math.round(personagem.atributos[i].valor / 2);
-            const proezasArray = await knex.select().table('proezas').where({ atributoid: personagem.atributos[i].id });
-            const proezasEmbaralhadas = embaralharArray(proezasArray);
-
-            for (let j = 0; j < proezasEmbaralhadas.length && pontosDeProeza != 0; j++) {
-                // Cleanup de proezas
-                cleanupProeza(proezasEmbaralhadas[j], personagem.atributos[i]);
-
-                // Seleção
-                if (proezasEmbaralhadas[j].poderosa && (pontosDeProeza - 2) > 0) {
-                    mago = proezasEmbaralhadas[j].nome === "Mago"
-
-                    personagem.proezas.push(proezasEmbaralhadas[j]);
-                    pontosDeProeza = pontosDeProeza - 2;
-                    j++;
-
-                } else if (!proezasEmbaralhadas[j].poderosa) {
-                    personagem.proezas.push(proezasEmbaralhadas[j]);
-                    pontosDeProeza = pontosDeProeza - 1;
-
-                } else if (pontosDeProeza === 0) {
+        for (let i = 0; i < atributosEmbaralhados.length; i++) {
+            switch (i) {
+                case 0:
+                    atributosEmbaralhados[i].valor = forte;
                     break;
+                case 1:
+                    atributosEmbaralhados[i].valor = medio;
+                    break;
+                case 2:
+                    atributosEmbaralhados[i].valor = fraco;
+                    break;
+                default:
+                    atributosEmbaralhados[i].valor = minimo
+            }
+
+            personagem.atributos.push(atributosEmbaralhados[i])
+
+            // Define Proezas
+            if (i <= 2) {
+                let pontosDeProeza = i === 2 ? 1 : Math.round(personagem.atributos[i].valor / 2);
+                const proezasArray = await knex.select().table('proezas').where({ atributoid: personagem.atributos[i].id });
+                const proezasEmbaralhadas = embaralharArray(proezasArray);
+
+                for (let j = 0; j < proezasEmbaralhadas.length && pontosDeProeza != 0; j++) {
+                    // Cleanup de proezas
+                    cleanupProeza(proezasEmbaralhadas[j], personagem.atributos[i]);
+
+                    // Seleção
+                    if (proezasEmbaralhadas[j].poderosa && (pontosDeProeza - 2) > 0) {
+                        mago = proezasEmbaralhadas[j].nome === "Mago"
+
+                        personagem.proezas.push(proezasEmbaralhadas[j]);
+                        pontosDeProeza = pontosDeProeza - 2;
+                        j++;
+
+                    } else if (!proezasEmbaralhadas[j].poderosa) {
+                        personagem.proezas.push(proezasEmbaralhadas[j]);
+                        pontosDeProeza = pontosDeProeza - 1;
+
+                    } else if (pontosDeProeza === 0) {
+                        break;
+                    }
                 }
             }
+            // Cleanup de atributos
+            cleanupAtributo(personagem.atributos[i]);
         }
-        // Cleanup de atributos
-        cleanupAtributo(personagem.atributos[i]);
+
+    } catch (error) {
+        console.log(error);
+        return;
     }
 
     // Cleanups de proezas e atributos
@@ -77,13 +85,30 @@ async function gerarPersonagem(forte = 5, medio = 4, fraco = 3, minimo = 2) {
         delete atributo["id"];
     }
 
+    console.log("Personagem gerado!")
+
     return personagem;
 }
 
-gerarPersonagem()
-    .then((char) => {
-        console.log(char.atributos)
-        console.log(char.proezas)
-        process.exit()
-    });
+const salvarPersonagem = async (personagem) => {
+
+    try {
+        const data = await fs.readFile("./personagensGerados.json");
+        const geradosArray = JSON.parse(data);
+
+        geradosArray.push(personagem);
+
+        await fs.writeFile("./personagensGerados.json", JSON.stringify(geradosArray));
+
+        console.log("Personagem salvo!")
+
+    } catch (error) {
+        console.log(error)
+        return
+    }
+
+    process.exit()
+}
+
+gerarPersonagem().then((personagem) => salvarPersonagem(personagem))
 
